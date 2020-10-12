463d7597e9342d38c3afe6531382a014ece6b95d
grafijoss <grafijoss@hotmail.com>
2020-10-11 21:45:58 -0500

    format files

--- firebase/client.js
+++ firebase/client.js
@@ -1,37 +1,37 @@
-import * as firebase from "firebase";
+import * as firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcO8blmPjPcTJxXLPXoZEwMY0pJoEbfT4",
-  authDomain: "devter-c8137.firebaseapp.com",
-  databaseURL: "https://devter-c8137.firebaseio.com",
-  projectId: "devter-c8137",
-  storageBucket: "devter-c8137.appspot.com",
-  messagingSenderId: "178554169643",
-  appId: "1:178554169643:web:4704320b993003d6f57d6e",
-  measurementId: "G-K2S49ZQTGV",
-};
+  apiKey: 'AIzaSyBcO8blmPjPcTJxXLPXoZEwMY0pJoEbfT4',
+  authDomain: 'devter-c8137.firebaseapp.com',
+  databaseURL: 'https://devter-c8137.firebaseio.com',
+  projectId: 'devter-c8137',
+  storageBucket: 'devter-c8137.appspot.com',
+  messagingSenderId: '178554169643',
+  appId: '1:178554169643:web:4704320b993003d6f57d6e',
+  measurementId: 'G-K2S49ZQTGV',
+}
 
-!firebase.apps.length && firebase.initializeApp(firebaseConfig);
+!firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
 const mapUserFromFirebaseAuthToUser = (user) => {
-  const { displayName, email, photoURL } = user;
-  console.log(user);
+  const { displayName, email, photoURL } = user
+  console.log(user)
   return {
     avatar: photoURL,
     username: displayName,
     email,
-  };
-};
+  }
+}
 
 export const onAuthStateChanged = (onChange) => {
   return firebase.auth().onAuthStateChanged((user) => {
-    const normalizerdUser = mapUserFromFirebaseAuthToUser(user);
-    onChange(normalizerdUser);
-  });
-};
+    const normalizerdUser = user ? mapUserFromFirebaseAuthToUser(user) : null
+    onChange(normalizerdUser)
+  })
+}
 
 export const loginWithGitHub = () => {
-  const gitHubProvider = new firebase.auth.GithubAuthProvider();
-  return firebase.auth().signInWithPopup(gitHubProvider);
+  const gitHubProvider = new firebase.auth.GithubAuthProvider()
+  return firebase.auth().signInWithPopup(gitHubProvider)
   // .then(mapUserFromFirebaseAuthToUser) // this line is going to execute automatically once signInWithPopup finishes
-};
+}
---
+++ pages/home/index.js
@@ -0,0 +1,14 @@
+export default function HomePage() {
+  return (
+    <>
+      <section>
+        <header>header</header>
+        <nav>nav</nav>
+      </section>
+      <style jsx>{`
+        height: 49px;
+        position: fixed;
+      `}</style>
+    </>
+  )
+}
