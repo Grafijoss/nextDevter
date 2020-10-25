e6e823c7008572e2df6b4eccc7d680193fbef007
grafijoss <grafijoss@hotmail.com>
2020-10-25 15:36:48 -0500

    recharging the timer every second

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 const DATE_UNITS = [
   ['day', 86400],
   ['hour', 3600],
@@ -13,17 +15,29 @@ const getDateDiffs = (timestamp) => {
     // desustructuracion - destructuring
     if (Math.abs(elapsed) > seconsInUnit || unit === 'second') {
       // Math.abs nos retorna el valor absoluto
-      const value = Math.round(elapsed / seconsInUnit)
+      const value = Math.floor(elapsed / seconsInUnit)
       return { value, unit }
     }
   }
 }
 
 export default function useTimeAgo(timestamp) {
-  const { value, unit } = getDateDiffs(timestamp)
+  // if we pass a method to useEffect just will execute the first time
+  // it is better to pass a function and not the execution of the function
+  const [timeAgo, setTimeAgo] = useState(() => getDateDiffs(timestamp))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTimeAgo = getDateDiffs(timestamp)
+      setTimeAgo(newTimeAgo)
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [timestamp])
+
   // relative time format
   const rtf = new Intl.RelativeTimeFormat('es', { style: 'long' }) // short, narrow, long
   // format recives two parameters val and unit
-  console.log(value, unit)
+
+  const { value, unit } = timeAgo
   return rtf.format(value, unit)
 }
